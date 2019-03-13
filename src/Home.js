c0340ab4f756be894d220bb6ffb200596ff5f3a1
Ben Wilson <bwilson@brandwatch.com>
2019-03-13 09:58:29 +0000

    Remove unused import

--- src/Home.js
+++ src/Home.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ProjectCard from './components/ProjectCard';
 import projectConfiguration from './projects/projectConfiguration';
 import Typography from '@material-ui/core/Typography';
-import { withStyles } from '@material-ui/core/styles';
 
 import './css/homepage.css';
 
